a0150f5e438d6440dab2a4be6fcb523eafe4b3dc
GAEL RICARDO MIZRAHI <49191896@ort>
2025-05-08 09:06:46 -0300

    controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,8 +13,26 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
-    {
-        return View();
+    
+  public IActionResult Index()
+        {
+            discos.infoDiscos(); // Asegúrate de inicializar una sola vez en una app real
+            ViewBag.discos = discos.canciones();
+            return View(discos);
+
+        }
+    public IActionResult InformacionDisco(int id)
+        {
+            if (Grupo.albumes.ContainsKey(id))
+            {
+                ViewBag.albumes = Grupo.albumes[id];
+              
+            }
+
+             return View("InformacionDisco",id);
+        }
+   
     }
-}
+
+   
+
